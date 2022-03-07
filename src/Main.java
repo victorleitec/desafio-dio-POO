53d4334d6b6920e409fc4b23f934da0b7e866fde
Victor Leite Costa <victorleitecosta@outlook.com.br>
2022-03-07 15:12:27 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -23,8 +21,43 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /* System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devFulano = new Dev();
+        devFulano.setNome("Fulano");
+        devFulano.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Fulano: " + devFulano.getConteudosInscritos());
+
+        devFulano.progredir();
+        devFulano.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Fulano: " + devFulano.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Fulano: " + devFulano.getConteudosConcluidos());
+        System.out.println("XP: " + devFulano.calcularTotalXp());
+
+        System.out.println("-----------");
+
+        Dev devVictor = new Dev();
+        devVictor.setNome("Victor");
+        devVictor.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Victor: " + devVictor.getConteudosInscritos());
+
+        devVictor.progredir();
+        devVictor.progredir();
+        devVictor.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Victor: " + devVictor.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Victor: " + devVictor.getConteudosConcluidos());
+        System.out.println("XP: " + devVictor.calcularTotalXp());
+
     }
 }
